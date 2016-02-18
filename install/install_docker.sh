f31f5166cffe7f59114e8a14ff8502c785a72d24
seungkyoo park <wapj2000@gmail.com>
2016-02-18 15:42:18 +0900

    수정 install_docker.sh

--- install/install_docker.sh
+++ install/install_docker.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 
-which `yum`
-IS_CENTOS=$?
 
+PLATFORM="centos"
+
+if [ -f /etc/lsb-release ]; then
+    PLATFORM="ubuntu"
+fi
+
+# EOF 사용시에 EOF들어가는거는 스페이스 넣으면 안됨
 install_centos() {
-    sudo yum update
-    sudo tee /etc/yum.repos.d/docker.repo <<-'EOF'
-    [dockerrepo]
-    name=Docker Repository
-    baseurl=https://yum.dockerproject.org/repo/main/centos/$releasever/
-    enabled=1
-    gpgcheck=1
-    gpgkey=https://yum.dockerproject.org/gpg
-    EOF
-    sudo yum install docker-engine
-    sudo service docker start
-    sudo docker run hello-world
+sudo yum update
+sudo tee /etc/yum.repos.d/docker.repo <<-EOF
+[dockerrepo]
+name=Docker Repository
+baseurl=https://yum.dockerproject.org/repo/main/centos/$releasever/
+enabled=1
+gpgcheck=1
+gpgkey=https://yum.dockerproject.org/gpg
+EOF
+sudo yum install docker-engine
+sudo service docker start
+sudo docker run hello-world
 }
 
 install_ubuntu() {
@@ -26,7 +31,7 @@ install_ubuntu() {
    
 }
 
-if [ IS_CENTOS = 0 ];then
+if [ "$PLATFORM" = "centos" ];then
     install_centos
     echo "FINISH INSTALL DOCKER"
 else
