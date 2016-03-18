b1cb6be76f0ad5190c4010f49bb6efa572508902
seungkyoo park <wapj2000@gmail.com>
2016-03-18 16:08:00 +0900

    install_mysqlclient_for_python.sh 수정

--- install/install_mysqlclient_for_python.sh
+++ install/install_mysqlclient_for_python.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 sudo apt-get install python-dev libmysqlclient-dev
+### mariadb를 쓰는경우는 아래의 패키지를 설치하자 
+### sudo apt-get install python-devi libmariadbclient-dev
 sudo apt-get install python3-dev
 pip install mysqlclient
