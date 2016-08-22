882fbab3e9b74b386dcd979214d6f7e5e5664174
wapj <seungkyoo@treenod.com>
2016-08-22 16:22:05 +0900

    yaml로 뽑아낼때 조건문 줄수 있게 수정

--- utils/dump_to_yaml.py
+++ utils/dump_to_yaml.py
@@ -6,14 +6,15 @@ if len(sys.argv) < 2:
     print("테이블 명을 입력해주세요.")
     print("usage : dumpto_yaml.py <table_name>")
     print("ex : dumpto_yaml.py mydb.users")
+    print("ex : dumpto_yaml.py mydb.users id=123")
     exit(1)
 
 connection = pymysql.connect(host='localhost', user='', password='', charset='utf8mb4', cursorclass=pymysql.cursors.DictCursor)
 table_name = sys.argv[1]
 field = []
 
-def select_desc():
 
+def select_desc():
     with connection.cursor() as cursor:
         sql = "desc " + table_name
         cursor.execute(sql)
@@ -30,16 +31,22 @@ def select_table():
 
     select_query = select_query[:len(select_query) -1] + " FROM " + table_name
 
+    # 조건문이 있는경우는 넣어줌
+    if len(sys.argv) > 2:
+        select_query = select_query + " WHERE " + sys.argv[2]
+
     with connection.cursor() as cursor:
         cursor.execute(select_query)
         result = cursor.fetchall()
         return result
 
+
 def dump_yaml():
-    print('product_info:')
+    print('%s:' % table_name)
     for row in select_table():
         print('-')
         for f in field:
             print("    " + f + ": " + str(row[f]))
+
 
 dump_yaml()
