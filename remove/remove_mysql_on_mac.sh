1c0230977a7d8a6c6376be4b009a4e6e63edaf16
wapj2000 <wapj2000@gmail.com>
2016-09-03 10:46:28 +0900

    mysql삭제 추가

---
+++ remove/remove_mysql_on_mac.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo rm /usr/local/mysql
+sudo rm -rf /usr/local/mysql*
+sudo rm -rf /Library/StartupItems/MySQLCOM
+sudo rm -rf /Library/PreferencePanes/My*
+rm -rf ~/Library/PreferencePanes/My*
+sudo rm -rf /Library/Receipts/mysql*
+sudo rm -rf /Library/Receipts/MySQL*
+sudo rm -rf /private/var/db/receipts/*mysql*
